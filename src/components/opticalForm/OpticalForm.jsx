a03b4056120acb6ba276687602f6540de815862c
Yakup Zengin <clafedersin@gmail.com>
2023-11-11 16:04:04 +0300

    Added Optical Form

--- src/components/opticalForm/OpticalForm.jsx
+++ src/components/opticalForm/OpticalForm.jsx
@@ -7,6 +7,7 @@ const OpticalForm = ({ selectedOptions }) => {
     <div className="optical-form">
       <div className='quiz-title'>Optik Form</div>
       <div className="answer-sheet">
+        
         {selectedOptions.map((selected, i) => (
           <div key={i} className="answer">
              <span style={{color:"darkmagenta",fontWeight:900,fontSize:17}}>Soru {i + 1}:</span>  {selected || '-'}
